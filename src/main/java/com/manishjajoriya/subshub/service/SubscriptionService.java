58f1c2cf02396e6d0738b5d2686e85c48ea6e933
Manish Jajoriya <manishjajoriya2005@gmail.com>
2025-06-27 11:05:23 +0530

    added @Autowired on constructor

--- src/main/java/com/manishjajoriya/subshub/service/SubscriptionService.java
+++ src/main/java/com/manishjajoriya/subshub/service/SubscriptionService.java
@@ -3,6 +3,7 @@ package com.manishjajoriya.subshub.service;
 import com.manishjajoriya.subshub.entity.SubscriptionEntity;
 import com.manishjajoriya.subshub.repository.SubscriptionRepo;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +11,7 @@ public class SubscriptionService {
 
   private final SubscriptionRepo subscriptionRepo;
 
+  @Autowired
   public SubscriptionService(SubscriptionRepo subscriptionRepo) {
     this.subscriptionRepo = subscriptionRepo;
   }
