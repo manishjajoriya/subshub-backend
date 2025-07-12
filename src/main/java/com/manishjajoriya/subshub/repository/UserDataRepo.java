337129263f19650b8aeff1039d2433b677c94588
Manish Jajoriya <manishjajoriya2005@gmail.com>
2025-07-12 20:02:48 +0530

    added supression for checkstyle

--- src/main/java/com/manishjajoriya/subshub/repository/UserDataRepo.java
+++ src/main/java/com/manishjajoriya/subshub/repository/UserDataRepo.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@SuppressWarnings("checkstyle:MethodName")
 public interface UserDataRepo extends JpaRepository<UserDataEntity, UUID> {
   List<UserDataEntity> findByUserUid(UUID userUid);
 
